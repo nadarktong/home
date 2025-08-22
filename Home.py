88495f34da082f173eb11639c3e2b408bd78e96d
nadarktong <neangez@gmail.com>
2025-08-22 12:01:24 +0700

    .

--- Home.py
+++ Home.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-
-st.page_link("page/KnnwithHeart.py", label="การวิเคราะห์การโจมตีแบบ Fishing", icon="1️⃣")
-st.page_link("page/NaiveBaye.py", label="การทำนายข้อมูลด้วยเทคนิค Naive Baye", icon="2️⃣", disabled=False)
-st.page_link("page/DTree.py", label="การวิเคราะห์การโจมตีแบบ Fishing", icon="1️⃣")
+st.page_link("pages/KnnwithHeart.py", label="การวิเคราะห์การโจมตีแบบ Fishing", icon="1️⃣")
+st.page_link("pages/NaiveBaye.py", label="การทำนายข้อมูลด้วยเทคนิค Naive Baye", icon="2️⃣", disabled=False)
+st.page_link("pages/DTree.py", label="การวิเคราะห์การโจมตีแบบ Fishing", icon="1️⃣")
